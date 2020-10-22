1690a6c90b77db976cb221341ffa01199ac1cf60
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-22 19:13:26 +0200

    personnalize box shadow

--- src/page/HomeLink.tsx
+++ src/page/HomeLink.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function HomeLink(props: Props) {
     return (
-        <Link to={ props.path } className="homepage-link transition duration-150 overflow-hidden relative flex border-4 border-white shadow-md hover:shadow-lg w-full bg-cover md:w-96 mx-auto my-4 bg-black text-white h-72">
+        <Link to={ props.path } className="homepage-link transition duration-150 overflow-hidden relative flex border-4 border-white shadow-md-dark hover:shadow-dark w-full bg-cover md:w-96 mx-auto my-4 bg-black text-white h-72">
             <img alt="homepage-link" className="absolute top-0 left-0 transition duration-300 w-full h-full" src={ props.imgPath }/>
             <div className='m-auto w-full h-full z-10 text-xl bg-black bg-opacity-50 flex'>
                 <div className="m-auto">{ props.text }</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,32 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {
-      width: {
-        '72' : '20rem',
-        '80' : '24rem',
-        '88' : '28rem',
-        '96' : '32rem',
-      },
-      height: {
-        '72' : '20rem',
-        '80' : '24rem',
-        '88' : '28rem',
-        '96' : '32rem',
-      },
-      backgroundImage: {
-        'mouse' : "url('/images/mouse.jpg')",
-        'keyboard' : "url('/images/keyboard.jpg')",
-      }
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
     },
-  },
-  variants: {},
-  plugins: [],
+    purge: [],
+    theme: {
+        extend: {
+            width: {
+                '72' : '20rem',
+                '80' : '24rem',
+                '88' : '28rem',
+                '96' : '32rem',
+            },
+            height: {
+                '72' : '20rem',
+                '80' : '24rem',
+                '88' : '28rem',
+                '96' : '32rem',
+            },
+            backgroundImage: {
+                'mouse' : "url('/images/mouse.jpg')",
+                'keyboard' : "url('/images/keyboard.jpg')",
+            },
+            boxShadow:{
+                'md-dark' : '2px 2px 10px gray'
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
 };
